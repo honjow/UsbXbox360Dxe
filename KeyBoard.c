dfda50f6286edfa83e3e21a249536afea8e856c0
honjow <honjow311@gmail.com>
2025-10-31 19:40:22 +0800

    fix: move button and direction macros to Xbox360Input.h
    - Move XBOX360_BUTTON_* macros from KeyBoard.c to Xbox360Input.h
    - Move STICK_DIR_* macros from KeyBoard.c to Xbox360Input.h
    - This allows Xbox360Input.c to use these definitions
    - Remove duplicate definitions from KeyBoard.c

--- KeyBoard.c
+++ KeyBoard.c
@@ -13,33 +13,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include "Xbox360Config.h"
 #include "Xbox360Input.h"
 
-//
-// Xbox 360 button bit definitions
-//
-#define XBOX360_BUTTON_DPAD_UP         BIT0
-#define XBOX360_BUTTON_DPAD_DOWN       BIT1
-#define XBOX360_BUTTON_DPAD_LEFT       BIT2
-#define XBOX360_BUTTON_DPAD_RIGHT      BIT3
-#define XBOX360_BUTTON_START           BIT4
-#define XBOX360_BUTTON_BACK            BIT5
-#define XBOX360_BUTTON_LEFT_THUMB      BIT6
-#define XBOX360_BUTTON_RIGHT_THUMB     BIT7
-#define XBOX360_BUTTON_LEFT_SHOULDER   BIT8
-#define XBOX360_BUTTON_RIGHT_SHOULDER  BIT9
-#define XBOX360_BUTTON_GUIDE           BIT10
-#define XBOX360_BUTTON_A               BIT12
-#define XBOX360_BUTTON_B               BIT13
-#define XBOX360_BUTTON_X               BIT14
-#define XBOX360_BUTTON_Y               BIT15
-
-//
-// Analog stick direction bit definitions
-//
-#define STICK_DIR_UP       BIT0
-#define STICK_DIR_DOWN     BIT1
-#define STICK_DIR_LEFT     BIT2
-#define STICK_DIR_RIGHT    BIT3
-
 USB_KEYBOARD_LAYOUT_PACK_BIN  mUsbKeyboardLayoutBin = {
   sizeof (USB_KEYBOARD_LAYOUT_PACK_BIN),   // Binary size
 
--- Xbox360Input.h
+++ Xbox360Input.h
@@ -19,6 +19,33 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Protocol/UsbIo.h>
 
+//
+// Xbox 360 button bit definitions
+//
+#define XBOX360_BUTTON_DPAD_UP         BIT0
+#define XBOX360_BUTTON_DPAD_DOWN       BIT1
+#define XBOX360_BUTTON_DPAD_LEFT       BIT2
+#define XBOX360_BUTTON_DPAD_RIGHT      BIT3
+#define XBOX360_BUTTON_START           BIT4
+#define XBOX360_BUTTON_BACK            BIT5
+#define XBOX360_BUTTON_LEFT_THUMB      BIT6
+#define XBOX360_BUTTON_RIGHT_THUMB     BIT7
+#define XBOX360_BUTTON_LEFT_SHOULDER   BIT8
+#define XBOX360_BUTTON_RIGHT_SHOULDER  BIT9
+#define XBOX360_BUTTON_GUIDE           BIT10
+#define XBOX360_BUTTON_A               BIT12
+#define XBOX360_BUTTON_B               BIT13
+#define XBOX360_BUTTON_X               BIT14
+#define XBOX360_BUTTON_Y               BIT15
+
+//
+// Analog stick direction bit definitions
+//
+#define STICK_DIR_UP       BIT0
+#define STICK_DIR_DOWN     BIT1
+#define STICK_DIR_LEFT     BIT2
+#define STICK_DIR_RIGHT    BIT3
+
 /**
   Handler function for Xbox 360 controller asynchronous interrupt transfer.
 
